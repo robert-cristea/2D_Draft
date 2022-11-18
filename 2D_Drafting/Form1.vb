21ca4fa6fb522ccfac2a932c27930942223a6e77
vietvulong <vietvulong@protonmail.com>
2022-11-18 13:33:59 +0700

    Solve Design Error

--- 2D_Drafting/Form1.vb
+++ 2D_Drafting/Form1.vb
@@ -136,22 +136,6 @@ Public Structure C_LineObject
     End Sub
 End Structure
 
-Public Class C_CurveObject
-    Public CurvePoint(1000) As PointF
-    Public CDrawPos As PointF
-    Public CPointIndx As Integer
-
-    Public Sub Refresh()
-        CDrawPos.X = 0
-        CDrawPos.Y = 0
-        CPointIndx = 0
-        For i = 0 To 1000
-            CurvePoint(i).X = 0
-            CurvePoint(i).Y = 0
-        Next
-    End Sub
-End Class
-
 Public Structure C_PointObject
     Public PointPoint As PointF
     Public PDrawPos As PointF
@@ -164,50 +148,6 @@ Public Structure C_PointObject
     End Sub
 End Structure
 
-Public Class C_PolyObject
-    Public PolyDrawPos As PointF
-    Public PolyPointIndx As Integer
-    Public PolyPoint(50) As PointF
-
-    Public Sub Refresh()
-        PolyPointIndx = 0
-        PolyDrawPos.X = 0
-        PolyDrawPos.Y = 0
-        For i = 0 To 50
-            PolyPoint(i).X = 0
-            PolyPoint(i).Y = 0
-        Next
-    End Sub
-End Class
-
-Public Class C_CuPolyObject
-    Public CuPolyPoint(30, 1000) As PointF
-    Public CuPolyDrawPos As PointF
-    Public CuPolyPointIndx_j As Integer
-    Public CuPolyPointIndx_k(30) As Integer
-
-    Public Sub Refresh()
-        CuPolyPointIndx_j = 0
-        CuPolyDrawPos.X = 0
-        CuPolyDrawPos.Y = 0
-        For i = 0 To 30
-            CuPolyPointIndx_k(i) = 0
-            For j = 0 To 1000
-                CuPolyPoint(i, j).X = 0
-                CuPolyPoint(i, j).Y = 0
-            Next
-        Next
-    End Sub
-End Class
-
-Public Class CurveObject
-    Public CuPolyItem As List(Of C_CuPolyObject) = New List(Of C_CuPolyObject)()
-    Public PolyItem As List(Of C_PolyObject) = New List(Of C_PolyObject)()
-    Public PointItem As List(Of C_PointObject) = New List(Of C_PointObject)()
-    Public LineItem As List(Of C_LineObject) = New List(Of C_LineObject)()
-    Public CurveItem As List(Of C_CurveObject) = New List(Of C_CurveObject)()
-
-End Class
 'structure for drawing objects
 Public Structure MeasureObject
     Public start_point As PointF        'start point of object
@@ -1997,8 +1937,7 @@ Public Class Main_Form
 
     End Sub
 
-#End Region
-#Region "Curves"
+
     ''' <summary>
     ''' set current measurement type as C_Line
     ''' </summary>
